701cbb6cac6e64e2837c31feb7dae15c7cd58151
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-11-15 17:10:04 +0100

    primo commit

---
+++ src/org/generation/italy/security/PasswordGenerator.java
@@ -0,0 +1,18 @@
+package org.generation.italy.security;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		String name=("Pinco");
+		String lastName=("Pallo");
+		String favouriteColor=("magenta");
+		short dayBirth= 12;
+		short monthBirth= 5;
+		short yearBirth= 1994;
+		System.out.println(name + "-" + lastName +"-" + favouriteColor + "-" + (dayBirth + monthBirth + yearBirth));
+		
+		
+
+	}
+
+}
